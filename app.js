fdbc4c98718e38843eb5c241851548e8c0899a46
kustarbek <kustarbekturdubekov6@gmail.com>
2024-10-25 16:11:36 +0600

    первый коммит

---
+++ app.js
@@ -0,0 +1,7 @@
+
+
+
+function updateHeading() {
+  const inputField = document.getElementById("inputField");
+  const displayText = document.getElementById("displayText");
+  displayText.textContent = inputField.value;}
